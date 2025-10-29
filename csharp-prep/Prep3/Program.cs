3455d6740a4ac8598b08e0d9c97cfe84f469823e
hay25019 <hay25019@byui.edu>
2025-10-29 11:05:59 -0600

    I made a program that asks the user for a "magic number" and then asks the user for a guess. If the guess is higher than the magic number, the program tells them to guess lower. If the guess if lower, it tells them to guess higher. It also tells them if they guess the number.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int number = int.Parse(magicNumber);
+        Console.Write("What is your guess? ");
+        string stringGuess = Console.ReadLine();
+        int guess = int.Parse(stringGuess);
+        if (guess > number)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guess < number)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
     }
 }
