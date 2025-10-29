73823254184f9f17a5d47dda7eee3bb903616399
hay25019 <hay25019@byui.edu>
2025-10-29 11:32:06 -0600

    I made a program that asks the user for a list of numbers, then adds all the numbers, tells you the total, the average, and the highest number.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int userNumber;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        } while (userNumber != 0);
+        int total = 0;
+        int largest = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        int average = total / numbers.Count;
+        Console.WriteLine($"The sum is {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
