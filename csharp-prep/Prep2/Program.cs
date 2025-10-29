e5ae6bc791cce823256456b56358177346df97e0
hay25019 <hay25019@byui.edu>
2025-10-29 09:11:47 -0600

    I added the first two steps for this assignment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeFromUser = Console.ReadLine();
+        int grade = int.Parse(gradeFromUser);
+        if (grade >= 90)
+        {
+            Console.WriteLine("Grade: A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Grade: B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Grade: C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Grade: D");
+        }
+        else
+        {
+            Console.WriteLine("Grade: F");
+        }
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Oh well, you can always try again.");
+        }
     }
 }
