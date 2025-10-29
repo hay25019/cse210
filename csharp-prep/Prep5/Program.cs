34fc6651ac5f7ea8f4eeb5266ba5630f1873dfb4
hay25019 <hay25019@byui.edu>
2025-10-29 12:31:02 -0600

    I made a program that asks the user for their name, their favorite number, and their birth year. It then squares the number and tells the user the squared number and how old the user is turning that year.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            return Console.ReadLine();
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        static void PromptUserBirthYear(out int birthYear)
+        {
+            Console.Write("Please enter the year you were born: ");
+            birthYear = int.Parse(Console.ReadLine());
+        }
+
+        static int SquareNumber(int number)
+        {
+            return (int)Math.Pow(number, 2);
+        }
+
+        static void DisplayResult(string userName, int squareNumber, int birthYear)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            int turningAge = currentDateTime.Year - birthYear;
+            Console.WriteLine($"{userName}, the square of your number is {squareNumber}");
+            Console.WriteLine($"{userName}, you will turn {turningAge} this year.");
+        }
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int birthYear;
+        PromptUserBirthYear(out birthYear);
+        int squareNumber = SquareNumber(userNumber);
+        DisplayResult(userName, squareNumber, birthYear);
     }
 }
