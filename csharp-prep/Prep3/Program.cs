4fbadacbbfc923cd7aa1a46c5dcf738f115b4adf
hay25019 <hay25019@byui.edu>
2025-10-29 11:08:16 -0600

    I added a while loop so that the user can keep guessing until they get it right.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,20 +7,25 @@ class Program
         Console.Write("What is the magic number? ");
         string magicNumber = Console.ReadLine();
         int number = int.Parse(magicNumber);
-        Console.Write("What is your guess? ");
-        string stringGuess = Console.ReadLine();
-        int guess = int.Parse(stringGuess);
-        if (guess > number)
+        bool guessRight = false;
+        while (guessRight == false)
         {
-            Console.WriteLine("Lower");
-        }
-        else if (guess < number)
-        {
-            Console.WriteLine("Higher");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
+            Console.Write("What is your guess? ");
+            string stringGuess = Console.ReadLine();
+            int guess = int.Parse(stringGuess);
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                guessRight = true;
+            }
         }
     }
 }
