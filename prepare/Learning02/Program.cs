e5072ca4b65209a61ed5a21ba9d2aa78ffaf3b24
hay25019 <hay25019@byui.edu>
2025-11-10 08:25:22 -0700

    Added a function in the Job class to display all the details and implemented it

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -7,9 +7,16 @@ class Program
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
-        Console.WriteLine(job1._jobTitle);
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
         Job job2 = new Job();
         job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
     }
 }
