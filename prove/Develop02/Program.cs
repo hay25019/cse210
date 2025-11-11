ff0156f5e5348c0344db4f730c374155f6c1f3c7
hay25019 <hay25019@byui.edu>
2025-11-11 15:13:18 -0700

    Finished making the Entry class and added it to the main program

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        bool go = true;
+        do
+        {
+            Console.WriteLine("Please select one of the following choices.");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            string reply = Console.ReadLine();
+            if (reply == "1")
+            {
+                Entry entry = new Entry();
+                entry.GetUserInput();
+            }
+            else if (reply == "5")
+            {
+                go = false;
+            }
+        } while (go == true);
     }
 }
