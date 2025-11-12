67e2675109f50ded356a9cc4739c0df330cefe80
hay25019 <hay25019@byui.edu>
2025-11-12 09:15:34 -0700

    I added the option to not have a prompt

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 public class Entry
 {
     public List<string> _prompts = ["What was your favorite thing today?", "What do you wish you could do-over from today?", "How many people did you serve today?", "What are some blessings you got today?", "How many times did you compliment someone?"];
@@ -5,9 +7,19 @@ public class Entry
     public string GetUserInput()
     {
         Random random = new Random();
-        int randomIndex = random.Next(_prompts.Count);
-        string prompt = _prompts[randomIndex];
-        Console.WriteLine(prompt);
+        Console.Write("Do you want a prompt(y/n)? ");
+        string yesNo = Console.ReadLine().ToLower();
+        string prompt = "";
+        if (yesNo == "n")
+        {
+            prompt = "(No prompt)";
+        }
+        else
+        {
+            int randomIndex = random.Next(_prompts.Count);
+            prompt = _prompts[randomIndex];
+            Console.WriteLine(prompt);
+        }
         string response = Console.ReadLine();
         DateTime currentDay = DateTime.Now;
         string dateText = currentDay.ToShortDateString();
