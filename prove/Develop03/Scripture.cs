158eec4b04498703e70831b3ece46a7d234d704b
hay25019 <hay25019@byui.edu>
2025-11-19 12:05:23 -0700

    I made another change so that it only blacks out words that aren't already black.

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,28 +1,31 @@
+using System.Linq.Expressions;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Security.Principal;
 
 public class Scripture
 {
-    private List<Word> wordList = [];
+    private Dictionary<int, Word> wordList = new Dictionary<int, Word>();
     private string stringScripture;
     private Reference scriptureReference;
     private Random random = new Random();
     public Scripture(string reference, string scripture)
     {
         string[] stringList = scripture.Split(" ");
-        foreach (string text in stringList)
+        for (int i = 0; i < stringList.Count(); i++)
         {
+            string text = stringList[i];
             Word word = new Word(text);
-            wordList.Add(word);
+            wordList.Add(i, word);
         }
         scriptureReference = new Reference(reference);
     }
     private void MakeStringFromList()
     {
         List<string> stringList = [];
-        foreach (Word word in wordList)
+        for (int i = 0; i < wordList.Count(); i++)
         {
+            Word word = wordList[i];
             stringList.Add(word.GetVisibleContent());
         }
         stringScripture = string.Join(" ", stringList);
@@ -31,7 +34,27 @@ public class Scripture
     {
         for (int i = 0; i < 3; i++)
         {
-            int index = random.Next(0, wordList.Count);
+            int index;
+            while (true)
+            {
+                index = random.Next(0, wordList.Count);
+                if (wordList[index].GetIsVisible() == false)
+                {
+                    if (CheckIsVisible() == false)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+                
+            }
             wordList[index].MakeDissapear();
         }
     }
@@ -39,8 +62,9 @@ public class Scripture
     {
         bool visible = true;
         List<Word> visibleWords = [];
-        foreach (Word word in wordList)
+        for (int i = 0; i < wordList.Count(); i++)
         {
+            Word word = wordList[i];
             if (word.GetIsVisible() == true)
             {
                 visibleWords.Add(word);
