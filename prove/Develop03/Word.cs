ed9cddb8a781b2b1d52f32a850c4bca955c14f87
hay25019 <hay25019@byui.edu>
2025-11-19 11:10:08 -0700

    Finished the Word class. (At least for now.)

--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -1,4 +1,23 @@
 public class Word
 {
-    
+    private string wordContent;
+    private string hiddenContent;
+    private string visibleContent;
+    private bool isVisible = true;
+
+    public Word(string text)
+    {
+        wordContent = text;
+        hiddenContent = new string('*', text.Length);
+        visibleContent = wordContent;
+    }
+    public void MakeDissapear()
+    {
+        isVisible = false;
+        visibleContent = hiddenContent;
+    }
+    public string GetVisibleContent()
+    {
+        return visibleContent;
+    }
 }
