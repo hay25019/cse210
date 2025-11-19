b44bdfd44469fdad9af8d85d27b600271d9650b5
hay25019 <hay25019@byui.edu>
2025-11-19 12:17:38 -0700

    I added more scriptures

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,7 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Scripture scripture = new Scripture("Proverbs 3:5-6", "Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge Him, and He shall direct thy paths.");
+        List<string> references = ["Proverbs 3:5-6", "John 14:15", "Moroni 7:6-8"];
+        List<string> scriptures = ["Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge Him, and He shall direct thy paths.", "If ye love Me, keep My commandments.", "For behold, God hath said a man being evil cannot do that which is good; for if he offereth a gift, or prayeth unto God, except he shall do it with real intent it profiteth him nothing. For behold, it is not counted unto him for righteousness. For behold, if a man being evil giveth a gift, he doeth it grudgingly; wherefore it is counted unto him the same as if he had retained the gift; wherefore he is counted evil before God."];
+        Random random = new Random();
+        int index = random.Next(0, scriptures.Count);
+        Scripture scripture = new Scripture(references[index], scriptures[index]);
         while (true)
         {
             scripture.DisplayScripture();
