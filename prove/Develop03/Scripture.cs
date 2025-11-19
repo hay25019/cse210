e9893279164aceaa8f5da88ac6d7c3ebc4b3affe
hay25019 <hay25019@byui.edu>
2025-11-19 10:59:19 -0700

    I added a Scripture class and a Word class.

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,15 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+public class Scripture
+{
+    private List<Word> wordList;
+    public Scripture(string scripture)
+    {
+        string[] stringList = scripture.Split(" ");
+        foreach (string text in stringList)
+        {
+            
+        }
+    }
+}
