d9849cce232e891f164357776a695beca54fa0b0
hay25019 <hay25019@byui.edu>
2025-11-24 15:53:47 -0700

    Added and tested the WritingAssignment class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -11,5 +11,9 @@ class Program
         MathAssignment mathAssignment = new MathAssignment("Ammon Hayward", "Fractions", "7.3", "8-19");
         Console.WriteLine(mathAssignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeWorkList());
+
+        WritingAssignment writingAssignment = new WritingAssignment("Ammon Hayward", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
