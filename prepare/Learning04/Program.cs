1f139a1f1a4aac2f5064caf62d405871c4fa8e1a
hay25019 <hay25019@byui.edu>
2025-11-24 15:47:46 -0700

    Made the MathAssignments class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 
 class Program
 {
@@ -6,5 +7,9 @@ class Program
     {
         Assignment assignment = new Assignment("Ammon Hayward", "Programming");
         Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathAssignment = new MathAssignment("Ammon Hayward", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeWorkList());
     }
 }
