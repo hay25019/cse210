3f4c2e4238d932db67cacb45bcee4221c061a069
hay25019 <hay25019@byui.edu>
2025-12-01 10:08:24 -0700

    Started making the Shape class

---
+++ prepare/Learning05/Shape.cs
@@ -0,0 +1,13 @@
+public class Shape
+{
+    private string _color;
+
+    public string GetColor()
+    {
+        return _color;
+    }
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+}
