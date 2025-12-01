f5ce519d3e2962ed1f8ab49576b5854e300fad14
hay25019 <hay25019@byui.edu>
2025-12-01 10:25:47 -0700

    Made and tested the Circle class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -11,5 +11,9 @@ class Program
         Rectangle rectangle = new Rectangle("green", 5, 6);
         Console.WriteLine(rectangle.GetColor());
         Console.WriteLine(rectangle.GetArea());
+
+        Circle circle = new Circle("blue", 3);
+        Console.WriteLine(circle.GetColor());
+        Console.WriteLine(circle.GetArea());
     }
 }
