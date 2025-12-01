7962f220389f85acf35f5e3e8901a4667e2383a8
hay25019 <hay25019@byui.edu>
2025-12-01 10:20:51 -0700

    Made and tested the Rectangle class

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -7,5 +7,9 @@ class Program
         Square square = new Square("red", 12);
         Console.WriteLine(square.GetColor());
         Console.WriteLine(square.GetArea());
+
+        Rectangle rectangle = new Rectangle("green", 5, 6);
+        Console.WriteLine(rectangle.GetColor());
+        Console.WriteLine(rectangle.GetArea());
     }
 }
