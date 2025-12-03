414b847168d82ea46d074d2b104d44d5bd104fff
hay25019 <hay25019@byui.edu>
2025-12-03 11:07:29 -0700

    Made the ChecklistGoal class

---
+++ prove/Develop05/ChecklistGoal.cs
@@ -0,0 +1,21 @@
+public class ChecklistGoal : Goal
+{
+    private int _timesToComplete;
+    private int _bonusValue;
+    private int _timesCompleted;
+    public ChecklistGoal(string name, string description, int pointValue, int bonusValue, int timesToComplete) : base(name, description, pointValue)
+    {
+        _bonusValue = bonusValue;
+        _timesToComplete = timesToComplete;
+    }
+    public override void RecordEvent()
+    {
+        _score += _pointValue;
+        _timesCompleted++;
+        if (_timesCompleted == _timesToComplete)
+        {
+            _score += _bonusValue;
+            _completed = true;
+        }
+    }
+}
