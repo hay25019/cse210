bdf9469fa33bcff6e0866c30fba3c585256042a6
hay25019 <hay25019@byui.edu>
2025-12-15 13:12:37 -0700

    Finished the program

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+        activities.Add(new Running("15 Dec 2025", 30, 2.5));
+        activities.Add(new Cycling("15 Dec 2025", 30, 7));
+        activities.Add(new Swimming("15 Dec 2025", 120, 200));
+
+        foreach (Activity activity in activities)
+        {
+            activity.GetSummary();
+        }
     }
 }
