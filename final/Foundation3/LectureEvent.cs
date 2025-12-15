cdebacabcdec5901edc3b6162f2b0113164705f4
hay25019 <hay25019@byui.edu>
2025-12-15 11:26:36 -0700

    Made the LectureEvent class

---
+++ final/Foundation3/LectureEvent.cs
@@ -0,0 +1,18 @@
+public class LectureEvent : Event
+{
+    private string _speaker;
+    private int _capacity;
+    public LectureEvent(string title, string description, string date, string time, Address address, string speaker, int capacity) : base(title, description, date, time, address)
+    {
+        _speaker = speaker;
+        _capacity = capacity;
+    }
+    public override void FullDetails()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"{_description}");
+        Console.WriteLine($"Speaker: {_speaker}");
+        Console.WriteLine($"Max seating: {_capacity}");
+        Console.WriteLine($"{_time} at {_address}");
+    }
+}
