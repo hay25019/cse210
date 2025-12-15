cd684a26f27a9169c2d08a6b10a823d187ce7a66
hay25019 <hay25019@byui.edu>
2025-12-15 11:30:57 -0700

    Made the OutdoorEvent class

---
+++ final/Foundation3/OutdoorEvent.cs
@@ -0,0 +1,16 @@
+public class OutdoorEvent : Event
+{
+    private string _weather;
+
+    public OutdoorEvent(string title, string description, string date, string time, Address address, string weather) : base(title, description, date, time, address)
+    {
+        _weather = weather;
+    }
+    public override void FullDetails()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"{_description}");
+        Console.WriteLine($"Weather: {_weather}");
+        Console.WriteLine($"{_time} at {_address}");
+    }
+}
