2f521e8d13248cf924a026fd9beb9454ff6090bf
hay25019 <hay25019@byui.edu>
2025-12-15 11:19:56 -0700

    Made the abstract Event class

---
+++ final/Foundation3/Event.cs
@@ -0,0 +1,37 @@
+using System.Net.Sockets;
+
+abstract class Event
+{
+    string _title;
+    string _description;
+    string _date;
+    string _time;
+    string _eventType;
+    Address _address;
+
+    public Event(string title, string description, string date, string time, Address address)
+    {
+        _title = title;
+        _description = description;
+        _date = date;
+        _time = time;
+        _address = address;
+    }
+    public void StandardDetails()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"{_description}");
+        Console.WriteLine($"{_time} at {_address}");
+    }
+    public virtual void FullDetails()
+    {
+        Console.WriteLine($"Title: {_title}");
+        Console.WriteLine($"{_description}");
+        Console.WriteLine($"{_time} at {_address}");
+    }
+    public void ShortDescription()
+    {
+        Console.WriteLine($"Type of event: {_eventType}");
+        Console.WriteLine($"Title: {_title}, date: {_date}");
+    }
+}
